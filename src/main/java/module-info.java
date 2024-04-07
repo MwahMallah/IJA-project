c80b9e5062dc68b698308f600e65304ba1af4bfe
Anastasiia <118450332+aanastsaa@users.noreply.github.com>
2024-04-07 19:55:35 +0200

    Update module-info.java, add export for GUI module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,7 @@ module org.vut.ija_project {
     exports org.vut.ija_project.Robot;
     exports org.vut.ija_project.Common;
     exports org.vut.ija_project.Obstacle;
+    
+    exports org.vut.ija_project.GUI;
+    opens org.vut.ija_project.GUI to javafx.fxml;
 }
